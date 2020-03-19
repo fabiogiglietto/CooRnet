ccab16ac1a754332c8e8930cf8898985a54557f6
Nicola Righetti <nicolarighetti79@gmail.com>
2020-03-19 15:18:43 +0100

    renaming of the argument ct_shares.df to avoid original file overwriting

--- R/get_outputs.R
+++ R/get_outputs.R
@@ -1,10 +1,10 @@
 get_outputs <- function(get_coord_shares_output,
-                        ct_shares.df = TRUE,
+                        ct_shares_marked.df = TRUE,
                         highly_connected_g = TRUE,
                         highly_connected_coordinated_entities = TRUE){
 
  if (ct_shares.df)
-    ct_shares.df <<- as.data.frame(get_coord_shares_output[[1]])
+    ct_shares_marked.df <<- as.data.frame(get_coord_shares_output[[1]])
  if (highly_connected_g)
     highly_connected_g <<- get_coord_shares_output[2][[1]]
  if (highly_connected_coordinated_entities)
