4a50577ff954d6a580b0431e2980207ff1365e83
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2020-03-19 14:48:41 +0100

    renamed output dataframe
    to avoid overwriting the original ct_shares.df

--- R/get_outputs.R
+++ R/get_outputs.R
@@ -4,7 +4,7 @@ get_outputs <- function(get_coord_shares_output,
                         highly_connected_coordinated_entities = TRUE){
 
  if (ct_shares.df)
-    ct_shares.df <<- as.data.frame(get_coord_shares_output[[1]])
+    ct_shares_marked.df <<- as.data.frame(get_coord_shares_output[[1]])
  if (highly_connected_g)
     highly_connected_g <<- get_coord_shares_output[2][[1]]
  if (highly_connected_coordinated_entities)
