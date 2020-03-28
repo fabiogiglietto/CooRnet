22d52d8cc05fef02165f94177c24b62e6fcd2856
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2020-03-28 15:46:46 +0000

    added clean_urls paramter
    to unnest_ctshares function

--- R/utils.R
+++ R/utils.R
@@ -31,13 +31,19 @@ clean_urls <- function(df, url){
   return(df)
 }
 
-unnest_ctshares <- function(ct_shares.df) {
+unnest_ctshares <- function(ct_shares.df, clean_urls=FALSE) {
   require(tidyr)      # 1.0.2
   
   # unnest expanded url and clean-up
   ct_shares.df <- unnest(ct_shares.df, cols = expandedLinks)
   ct_shares.df$original <- NULL
   
+  # clean urls
+  if(clean_urls==TRUE){
+    ct_shares.df <- clean_urls(ct_shares.df, "expanded")
+  }
+  
   # remove duplicates created by the unnesting
   ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),]
+  return(ct_shares.df)
 }
