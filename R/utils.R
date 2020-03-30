a3f6ed4045395e4596e24ed9f5db317886f42b53
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2020-03-30 16:51:39 +0200

    added check on share date
    removed shares performed more than one week from first

--- R/utils.R
+++ R/utils.R
@@ -45,6 +45,14 @@ unnest_ctshares <- function(ct_shares.df, clean_urls=FALSE) {
 
   # remove duplicates created by the unnesting
   ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),]
+  
+  # remove shares performed more than one week from first share
+  ct_shares.df_1 <- ct_shares.df %>%
+  group_by(expanded) %>%
+  mutate(first_to_last = max(date)-min(date)) %>%
+  filter(first_to_last <= 604800) %>%
+  select(-first_to_last)
+  
   return(ct_shares.df)
 }
 
