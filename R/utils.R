ab7fe68dbaac7a3648a3550e17ff27c8a09a2e1e
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2020-03-30 21:00:38 +0200

    renamed subsetted df

--- R/utils.R
+++ R/utils.R
@@ -47,7 +47,7 @@ unnest_ctshares <- function(ct_shares.df, clean_urls=FALSE) {
   ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),]
 
   # remove shares performed more than one week from first share
-  ct_shares.df3c <- ct_shares.df %>%
+  ct_shares.df <- ct_shares.df %>%
     group_by(expanded) %>%
     filter(difftime(max(date), min(date), units = "secs") <= 604800)
 
