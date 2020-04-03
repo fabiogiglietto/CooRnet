660dffb41ff662b556675a2a7f657722ab907128
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-04-03 12:33:56 +0000

    removed the unnest function

--- R/utils.R
+++ R/utils.R
@@ -31,30 +31,6 @@ clean_urls <- function(df, url){
   return(df)
 }
 
-unnest_ctshares <- function(ctshares_output, clean_urls=FALSE) {
-  require(tidyr)      # 1.0.2
-
-  # unnest expanded url and clean-up
-  ct_shares.df <- ctshares_output[[1]]
-  ct_shares.df <- unnest(ct_shares.df, cols = expandedLinks)
-  ct_shares.df$original <- NULL
-
-  # clean urls
-  if(clean_urls==TRUE){
-    ct_shares.df <- clean_urls(ct_shares.df, "expanded")
-  }
-
-  # remove duplicates created by the unnesting
-  ct_shares.df <- ct_shares.df[!duplicated(ct_shares.df[,c("id", "platformId", "postUrl", "expanded")]),]
-
-  # remove shares performed more than one week from first share
-  ct_shares.df <- ct_shares.df %>%
-    group_by(expanded) %>%
-    filter(difftime(max(date), min(date), units = "secs") <= 604800)
-
-  return(ct_shares.df)
-}
-
 build_coord_graph <- function(ct_shares.df, coordinated_shares, percentile_edge_weight=0.90) {
 
   ###########################################
