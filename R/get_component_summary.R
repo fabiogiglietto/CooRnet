5ca8ca87434dd4e9418542f282b44116e29de96e
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-05-20 06:26:01 +0000

    removed NewsGuard score from component summary because their term of service explicitly denies programmatic use of their data

--- R/get_component_summary.R
+++ R/get_component_summary.R
@@ -9,9 +9,7 @@
 #' the proportion of coordinated shares over the total shares (coorshare_ratio), the average coordinated score (avg_cooRscore),
 #' a measure of dispersion (gini) in the distribution of domains coordinatedly shared by the component (0-1). Higher values correspond to an higher concentration (less different domains linked),
 #' the top 5 coordinatedly shared domains (ranked by n. of shares),
-#' the total number coordinatedly shared of domains,
-#' the \href{https://www.newsguardtech.com/}{News Guard} average \href{https://www.newsguardtech.com/ratings/rating-process-criteria/}{score} of the domains shared by the components,
-#' the proportion of unique domains that have been rated by News Guard
+#' the total number coordinatedly shared of domains
 #'
 #'
 #' @details The gini values are computed by using the Gini coefficient on the proportions of unique domains each component shared. The Gini coefficient is a measure of the degree of concentration (inequality) of a variable in a distribution.
@@ -54,53 +52,6 @@ get_component_summary <- function(output){
                                by.x = "account.url",
                                by.y = "name")
 
-
-  # query the the newsguardtech.com API
-  domain_score <- data.frame(parent_domain = unique(ct_shares_marked.df$parent_domain), newsguard_score = NA)
-
-  cat("\nGetting domains rating from NewsGuard (https://www.newsguardtech.com)...\n")
-
-  total <- nrow(domain_score) # progress bar
-  pb <- utils::txtProgressBar(min = 0, max = total, width = 100, style = 3)
-
-  for (i in 1:nrow(domain_score)) {
-
-    utils::setTxtProgressBar(pb, pb$getVal()+1)
-
-    parent_domain <- domain_score$parent_domain[i]
-
-    query <- httr::GET(paste0("https://api.newsguardtech.com/check/", parent_domain))
-
-    tryCatch(
-      {
-        if (query$status_code == 200) {
-          json <- httr::content(query, as = "text", encoding = "UTF-8")
-          c <- jsonlite::fromJSON(json, flatten = TRUE)
-          if (length(c$score) > 0) {
-            domain_score$newsguard_score[i] <- c$score
-          }
-        }
-        else {
-          print(paste(query$status_code, i))
-          write(paste("Unexpected http response code by the News Guard api", query$status_code, "on domain", parent_domain), file = "log.txt", append = TRUE)
-        }
-      },
-      error=function(cond) {
-        print(paste("Error:", message(cond), "on URL:", i))
-        write(paste("Error:", message(cond), "on URL:", i), file = "log.txt", append = TRUE)
-      },
-      finally={
-        Sys.sleep(0.01)
-      })
-  }
-
-  cat("\nAlmost done...\n")
-
-  # add the newsguardtech.com score to the domains
-  ct_shares_marked.df <- merge(x=ct_shares_marked.df,
-                               y=domain_score,
-                               by="parent_domain")
-
   # summarise
   summary_entities <- highly_connected_coordinated_entities %>%
     group_by(component) %>%
@@ -112,10 +63,7 @@ get_component_summary <- function(output){
   summary_domains <- ct_shares_marked.df %>%
     group_by(component) %>%
     summarise(unique.full_domain = length(unique(full_domain)),
-              unique.parent_domain = length(unique(parent_domain)),
-              newsguard.parent_domain.score = mean(newsguard_score, na.rm = T),
-              newsguard.parent_domain.rated = length(unique(parent_domain[!is.na(newsguard_score)]))) %>%
-    mutate(newsguard.parent_domain.prop = newsguard.parent_domain.rated/unique.parent_domain)
+              unique.parent_domain = length(unique(parent_domain)))
 
   summary <- merge(summary_entities, summary_domains, by = "component")
   rm(summary_entities, summary_domains)
