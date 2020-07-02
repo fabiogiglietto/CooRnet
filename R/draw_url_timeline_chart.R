a010909b68507c97bbe8e350f0ff54fc9226b5c4
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-07-02 08:36:17 +0000

    Check if history is available in the ct_shares before using it. If history is missing, cumulative shares count is calculated based on post shares.

--- R/draw_url_timeline_chart.R
+++ R/draw_url_timeline_chart.R
@@ -16,7 +16,7 @@
 #'   df <- get_top_news(output, order_by = "engagement", top=10)
 #'
 #' @importFrom tidyr unnest
-#' @importFrom dplyr group_by mutate arrange %>%
+#' @importFrom dplyr group_by mutate arrange %>% bind_rows filter ungroup select
 #' @importFrom stringr str_trim
 #' @importFrom igraph components induced.subgraph V
 #' @importFrom lubridate as_datetime
@@ -24,6 +24,7 @@
 #' @importFrom plotly ggplotly layout
 #' @importFrom ggsci scale_colour_startrek
 #' @importFrom imputeTS na.interpolation
+#' @importFrom stats lag
 #'
 #' @export
 
@@ -34,11 +35,16 @@ draw_url_timeline_chart <- function(output, top_coord_urls=NULL, top_url_id=1) {
   cl <- igraph::components(g)
   gc <- igraph::induced.subgraph(g, which(cl$membership == which.max(cl$csize)))
 
+  # check if top_coord_urls had been provided
   if (is.null(top_coord_urls)) {
+    # nope
     cat("\n\nBuilding a table of top shared URLs...")
     top_url <- CooRnet::get_top_coord_urls(output = output, order_by = "statistics.actual.shareCount", component = FALSE, top = 10)
   }
-  else {top_url <- top_coord_urls}
+  else {
+    # yep
+    top_url <- top_coord_urls
+    }
 
   top_url_tab <- top_url
   top_url$cooR.account.name <- strsplit(top_url$cooR.account.name,",")
@@ -49,34 +55,44 @@ draw_url_timeline_chart <- function(output, top_coord_urls=NULL, top_url_id=1) {
 
   t <- ct_shares_marked.df[ct_shares_marked.df$expanded == top_url_tab$expanded[top_url_id],]
 
-  t2 <- t[c("account.name", "expanded", "date", "subscriberCount", "history", "statistics.actual.shareCount", "is_coordinated")]
+  t2 <- t[c("account.name", "expanded", "date", "subscriberCount", "statistics.actual.shareCount", "is_coordinated")]
 
-  url.history <- bind_rows(t2$history, .id = "post_label")
+  if("history" %in% colnames(t)) {
+    cat("Analyzing historical engagement data...\n");
+    t2 <- t[c("account.name", "expanded", "date", "subscriberCount", "history", "statistics.actual.shareCount", "is_coordinated")]
 
-  shares_cumsum <- url.history %>%
-    # calculate shares difference between consecutive timesteps
-    group_by(post_label) %>%
-    arrange(timestep) %>%
-    mutate(diff_shares = actual.shareCount - lag(actual.shareCount, default = 0)) %>%
-    ungroup() %>%
-    mutate(date = as.POSIXct(date)) %>%
-    arrange(date) %>%
-    mutate(cum_shares = cumsum(diff_shares)) %>%
-    filter(difftime(date, min(date), units = "secs") <= 604800) %>% # keep only one week hist
-    select(date, cum_shares)
+    url.history <- dplyr::bind_rows(t2$history, .id = "post_label")
+
+    shares_cumsum <- url.history %>%
+      # calculate shares difference between consecutive timesteps
+      group_by(post_label) %>%
+      arrange(timestep) %>%
+      mutate(diff_shares = actual.shareCount - stats::lag(actual.shareCount, default = 0)) %>%
+      ungroup() %>%
+      mutate(date = as.POSIXct(date)) %>%
+      arrange(date) %>%
+      mutate(cum_shares = cumsum(diff_shares)) %>%
+      filter(difftime(date, min(date), units = "secs") <= 604800) %>% # keep only one week hist
+      select(date, cum_shares)
+  }
 
   t2 <- t2 %>% dplyr::arrange(date)
   t2 <- t2 %>% dplyr::group_by() %>% dplyr::mutate(shares=cumsum(statistics.actual.shareCount))
   t2$date <- lubridate::as_datetime(t2$date)
   t2$is_coordinated <- factor(t2$is_coordinated, ordered =T, levels = c("TRUE","FALSE"), labels = c("coordinated", "not coordinated"))
 
-  temp <- t2["date"]
-  temp <- bind_rows(temp, shares_cumsum)
-  temp <- arrange(temp, date)
-  temp <- imputeTS::na.interpolation(temp, option = "spline")
+  if("history" %in% colnames(t)) {
+    # add historicaly data
+    temp <- t2["date"]
+    temp <- dplyr::bind_rows(temp, shares_cumsum)
+    temp <- dplyr::arrange(temp, date)
 
-  t2 <- merge(temp, t2, by = "date")
-  rm(temp)
+    # estimates shares at post time
+    temp <- imputeTS::na.interpolation(temp, option = "spline")
+
+    t2 <- merge(temp, t2, by = "date")
+    rm(temp)
+  }
 
   p <- ggplot2::ggplot(data = t2, ggplot2::aes(x=date, y=cum_shares, label=account.name)) +
     ggplot2::geom_line(color="gray")+
