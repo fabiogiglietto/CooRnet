bb45f452a8682f16774bf13753acc38c8eaa419a
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-08-07 12:20:28 +0000

    added a parameter to support new CSV columun format

--- R/get_urls_from_ct_histdata.R
+++ R/get_urls_from_ct_histdata.R
@@ -3,6 +3,7 @@
 #' Given a CSV created by the CrowdTangle Historical Data feature (preferbly filtered for link type posts), this function extract a unique list of URLs with a first shared date
 #'
 #' @param ct_histdata_csv a local or remote link to a CSV created by the CrowdTangle Historical Data
+#' @param newformat set to TRUE in order to use new CrowdTangle CSV format
 #'
 #' @return A data.frame with a unique list of URLs with respective first seen date
 #'
@@ -19,7 +20,7 @@
 #'
 #' @export
 
-get_urls_from_ct_histdata <- function(ct_histdata_csv=NULL) {
+get_urls_from_ct_histdata <- function(ct_histdata_csv=NULL, newformat=FALSE) {
 
   if(is.null(ct_histdata_csv)) {
     stop("The function requires a valid CSV local or remote link to run")
@@ -27,19 +28,36 @@ get_urls_from_ct_histdata <- function(ct_histdata_csv=NULL) {
 
 cat("\nLoading CSV...")
 
-df <- read_csv(col_types = cols(
+if (newformat == TRUE) {
+  df <- read_csv(col_types = cols(
+    .default = col_skip(),
+    date = col_character(),
+    expandedLinks.original = col_character(),
+    expandedLinks.expanded = col_character()),
+    file =  ct_histdata_csv)
+
+  df$url <- ifelse(is.na(df$expandedLinks.expanded), df$expandedLinks.original, df$expandedLinks.expanded) # keep expanded links only
+
+  urls <- df %>%
+    group_by(url) %>%
+    summarise(date = min(date)) %>%
+    select(url, date)
+
+} else {
+  df <- read_csv(col_types = cols(
   .default = col_skip(),
   Created = col_character(),
   Link = col_character(),
   `Final Link` = col_character()),
   file =  ct_histdata_csv)
 
-df$url <- ifelse(is.na(df$`Final Link`), df$Link, df$`Final Link`) # keep expanded links only
+  df$url <- ifelse(is.na(df$`Final Link`), df$Link, df$`Final Link`) # keep expanded links only
 
-urls <- df %>%
-  group_by(url) %>%
-  summarise(Created = min(Created)) %>%
-  select(url, date=Created)
+  urls <- df %>%
+    group_by(url) %>%
+    summarise(Created = min(Created)) %>%
+    select(url, date=Created)
+}
 
 attr(urls, 'spec') <- NULL
 
