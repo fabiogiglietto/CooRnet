a3e1edfea85c0c69be40e4203790ba281edb8ec4
Fabio Giglietto <fabio.giglietto@gmail.com>
2020-08-18 11:59:37 +0000

    improved clean_url

--- R/utils.R
+++ R/utils.R
@@ -1,34 +1,45 @@
 clean_urls <- function(df, url){
 
   df <- df[!grepl("\\.\\.\\.$", df[[url]]),]
+  df <- df[!grepl("/url?sa=t&source=web", df[[url]], fixed=TRUE),]
 
   paramters_to_clean <- paste("\\?utm_.*",
-                                 "\\?ref.*",
-                                 "\\?fbclid.*",
-                                 "\\?rss.*",
-                                 "\\?ico.*",
-                                 "\\?recruiter.*",
-                                 "\\?sr_share_.*",
-                                 "\\?fb_rel.*",
-                                 "\\?social.*",
-                                 "\\?intcmp_.*",
-                                 "\\?xrs.*",
-                                 "\\?CMP.*",
-                                 "\\?tid.*",
-                                 "\\?ncid.*",
-                                 "&utm_.*",
-                                 "\\?rbs&utm_hp_ref.*",
-                                 "/#\\..*",
-                                 "\\?mobile.*",
-                                 "&fbclid.*",
-                                 "/$",
-                                 sep = "|")
+                              "feed_id.*",
+                              "&_unique_id.*",
+                              "\\?#.*",
+                              "\\?ref.*",
+                              "\\?fbclid.*",
+                              "\\?rss.*",
+                              "\\?ico.*",
+                              "\\?recruiter.*",
+                              "\\?sr_share_.*",
+                              "\\?fb_rel.*",
+                              "\\?social.*",
+                              "\\?intcmp_.*",
+                              "\\?xrs.*",
+                              "\\?CMP.*",
+                              "\\?tid.*",
+                              "\\?ncid.*",
+                              "&utm_.*",
+                              "\\?rbs&utm_hp_ref.*",
+                              "/#\\..*",
+                              "\\?mobile.*",
+                              "&fbclid.*",
+                              "/$",
+                              sep = "|")
 
+  df[[url]] <- gsub(paramters_to_clean, "", df[[url]])
+  df[[url]] <- gsub(paramters_to_clean, "", df[[url]])
   df[[url]] <- gsub(paramters_to_clean, "", df[[url]])
 
   df[[url]] <- gsub(".*(http)", "\\1", df[[url]]) # delete all before "http"
+  df[[url]] <- gsub("\\/$", "", df[[url]]) # delete remaining trailing slash
+  df[[url]] <- gsub("\\&$", "", df[[url]]) # delete remaining trailing &
+
   df <- df[!grepl("^http://127.0.0.1", df[[url]]), ]
   df <- df[grepl("http://|https://", df[[url]]),] # remove all the entries with the url that does not start with "http"
+
+
 
   return(df)
 }
