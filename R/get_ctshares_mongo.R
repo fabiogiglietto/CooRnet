6ed142cedc98aaa8e8499bee6b7c6071930b4ee0
Fabio Giglietto <fabiogiglietto@users.noreply.github.com>
2022-02-24 17:26:19 +0100

    removed search in query strings from mongo version

--- R/get_ctshares_mongo.R
+++ R/get_ctshares_mongo.R
@@ -163,7 +163,6 @@ get_ctshares_mongo <- function(urls,
                            "&includeSummary=FALSE",
                             ifelse(get_history, "&includeHistory=TRUE", "&includeHistory=FALSE"), # history takes a lot of space on the db
                            "&sortBy=date",
-                           "&searchField=Include_query_strings",
                            "&token=", Sys.getenv("CROWDTANGLE_API_KEY"),
                            "&count=", nmax)
 
